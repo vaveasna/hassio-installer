de317a21ec70be9baad56667a617ea54789d431a
[PT]GOODVIBE[PT] <tv4you2016@gmail.com>
2022-03-20 01:27:12 +0000

    Update hassio_s9xxarmbian.sh

--- hassio_s9xxarmbian.sh
+++ hassio_s9xxarmbian.sh
@@ -39,131 +39,97 @@ os_agent_version="1.2.2";
 # ==============================================================================
 
 
-
-
-# ------------------------------------------------------------------------------
-# Installs the os-agent 
-# ------------------------------------------------------------------------------
-install_os-agent() {
-
-  echo "# ------------------------------------------------------------------------------"
-  echo "Installing os-agent V${os_agent_version}..."
-  echo "# ------------------------------------------------------------------------------"
-
-  wget -c https://github.com/home-assistant/os-agent/releases/download/${os_agent_version}/os-agent_${os_agent_version}_linux_aarch64.deb
-
-  echo "# ------------------------------------------------------------------------------"
-  echo "dpkg os-agent V${os_agent_version}..."
-  echo "# ------------------------------------------------------------------------------"
-  dpkg -i os-agent_${os_agent_version}_linux_aarch64.deb
-
-
-}
-
-
-
+# ==============================================================================
+# Are we root?
+# ==============================================================================
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  echo "Please try again after running:"
+  echo "  sudo su"
+  exit 1
+fi
 
 # ------------------------------------------------------------------------------
 # Ensures the hostname of the Pi is correct.
 # ------------------------------------------------------------------------------
-update_hostname() {
-  old_hostname=$(< /etc/hostname)
-  if [[ "${old_hostname}" != "${HOSTNAME}" ]]; then
-    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
-    sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
-    hostname "${HOSTNAME}"
-    echo "Hostname will be changed on next reboot: ${HOSTNAME}"
-  fi
-}
+old_hostname=$(< /etc/hostname)
+if [[ "${old_hostname}" != "${HOSTNAME}" ]]; then
+  sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hostname
+  sed -i "s/${old_hostname}/${HOSTNAME}/g" /etc/hosts
+  hostname "${HOSTNAME}"
+  echo "Hostname will be changed on next reboot: ${HOSTNAME}"
+fi
 
-# ------------------------------------------------------------------------------
-# Installs all required software packages and tools
-# ------------------------------------------------------------------------------
-install_requirements() {
-  echo "# ------------------------------------------------------------------------------"
-  echo "Updating APT packages list..."
-  echo "# ------------------------------------------------------------------------------"
-  apt-get clean
-  rm -rf /var/lib/apt/lists/*
-  apt-get clean
-  apt --fix-broken install -y
-  apt-get update 
-  apt-get upgrade
-  apt-get install software-properties-common
-  apt-get update
-  echo "# ------------------------------------------------------------------------------"
-  echo "Ensure all requirements are installed..."
-  echo "# ------------------------------------------------------------------------------"
+
+echo "# ------------------------------------------------------------------------------"
+echo "Updating APT packages list..."
+echo "# ------------------------------------------------------------------------------"
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+apt-get clean
+apt --fix-broken install -y
+apt-get update 
+apt-get upgrade
+apt-get install software-properties-common
+apt-get update
+
+echo "# ------------------------------------------------------------------------------"
+echo "Ensure all requirements are installed..."
+echo "# ------------------------------------------------------------------------------"
+apt-get install -y "${REQUIREMENTS[@]}"
  
-  apt-get install -y "${REQUIREMENTS[@]}"
- 
-  
-}
-
-
 
 # ------------------------------------------------------------------------------
-# Installs the Docker engine
+# Installs the os-agent 
 # ------------------------------------------------------------------------------
-install_docker() {
-  echo "Installing Docker..."
-  curl -fsSL https://get.docker.com | sh
-}
+
+echo "# ------------------------------------------------------------------------------"
+echo "Installing os-agent V${os_agent_version}..."
+echo "# ------------------------------------------------------------------------------"
+
+wget -c https://github.com/home-assistant/os-agent/releases/download/${os_agent_version}/os-agent_${os_agent_version}_linux_aarch64.deb
+
+echo "# ------------------------------------------------------------------------------"
+echo "dpkg os-agent V${os_agent_version}..."
+echo "# ------------------------------------------------------------------------------"
+dpkg -i os-agent_${os_agent_version}_linux_aarch64.deb
+
+{
+  echo -e "\n[device]";
+  echo "wifi.scan-rand-mac-address=no";
+  echo -e "\n[connection]";
+  echo "wifi.clone-mac-address=preserve";
+} >> "/etc/NetworkManager/NetworkManager.conf"
+
+# ------------------------------------------------------------------------------
+# Installing Docker...
+# ------------------------------------------------------------------------------
+echo "# ------------------------------------------------------------------------------"
+echo "Installing Docker..."
+echo "# ------------------------------------------------------------------------------"
+curl -fsSL https://get.docker.com | sh
+
 
 # ------------------------------------------------------------------------------
 # Installs and starts Hass.io
 # ------------------------------------------------------------------------------
-install_hassio() {
-  echo "Installing Hass.io..."
-  wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
-  dpkg -i homeassistant-supervised.deb
-  sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+echo "# ------------------------------------------------------------------------------"
+echo "Installing Hass.io..."
+echo "# ------------------------------------------------------------------------------"
+wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+dpkg -i homeassistant-supervised.deb
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 
-}
+# Friendly closing message
+ip_addr=$(hostname -I | cut -d ' ' -f1)
+echo "======================================================================="
+echo "Hass.io is now installing Home Assistant."
+echo "This process may take up to 20 minutes. Please visit:"
+echo "http://${HOSTNAME}.local:8123/ in your browser and wait"
+echo "for Home Assistant to load."
+echo "If the previous URL does not work, please try http://${ip_addr}:8123/"
+journalctl -f
 
-# ------------------------------------------------------------------------------
-# Configure network-manager to disable random MAC-address on Wi-Fi
-# ------------------------------------------------------------------------------
-config_network_manager() {
-  {
-    echo -e "\n[device]";
-    echo "wifi.scan-rand-mac-address=no";
-    echo -e "\n[connection]";
-    echo "wifi.clone-mac-address=preserve";
-  } >> "/etc/NetworkManager/NetworkManager.conf"
-}
 
-# ==============================================================================
-# RUN LOGIC
-# ------------------------------------------------------------------------------
-main() {
-  # Are we root?
-  if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
-    echo "Please try again after running:"
-    echo "  sudo su"
-    exit 1
-  fi
-
-  # Install ALL THE THINGS!
-  update_hostname
-  install_requirements
-  install_os-agent
-  config_network_manager
-  install_docker
-  install_hassio
-
-  # Friendly closing message
-  ip_addr=$(hostname -I | cut -d ' ' -f1)
-  echo "======================================================================="
-  echo "Hass.io is now installing Home Assistant."
-  echo "This process may take up to 20 minutes. Please visit:"
-  echo "http://${HOSTNAME}.local:8123/ in your browser and wait"
-  echo "for Home Assistant to load."
-  echo "If the previous URL does not work, please try http://${ip_addr}:8123/"
-  journalctl -f
-  exit 0
-}
-main
 
